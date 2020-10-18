d5c491ae380689c9c03e878f42555800321dec59
Bleck <overlordbleck@gmail.com>
2020-10-18 18:34:43 -0400

    Fixing file structure

--- lua/autorun/sh_dcfl.lua
+++ lua/autorun/sh_dcfl.lua
@@ -1,11 +1,11 @@
 if SERVER then
-    AddCSLuaFile( "core/client/cl_detached_timer.lua" )
-    AddCSLuaFile( "core/client/cl_freelook.lua" )
+    AddCSLuaFile( "freelook/core/client/cl_detached_timer.lua" )
+    AddCSLuaFile( "freelook/core/client/cl_freelook.lua" )
 
-    include( "core/server/sv_init.lua" )
+    include( "freelook/core/server/sv_init.lua" )
 end
 
 if CLIENT then
-    include( "core/client/cl_detached_timer.lua" )
-    include( "core/client/cl_freelook.lua" )
+    include( "freelook/core/client/cl_detached_timer.lua" )
+    include( "freelook/core/client/cl_freelook.lua" )
 end
